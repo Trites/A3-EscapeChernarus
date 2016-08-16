06de3e954cc504685556bec61bf69b61239728d4
Trites <Gustaf.Svanerud@gmail.com>
2016-08-16 16:57:27 +0200

    Initial commit

---
+++ include/defines.hpp
@@ -0,0 +1,15 @@
+#define VERSION "1.7.5"
+#define MISSIONNAME "Escape Chernarus CUP-US TritesTweak"
+#define MISSIONNAMEFULL "co10 Escape Chernarus CUP-US 1.7.5 TritesTweak"
+
+//--- [Controls Style Preprocessors]
+#define ST_LEFT 				0
+#define ST_RIGHT 				1
+#define ST_CENTER 				2
+#define ST_MULTI 				16
+#define ST_PICTURE 				48
+#define ST_TEXT_BG 				128
+#define ST_LINE 				176
+#define ST_KEEP_ASPECT_RATIO	"0x30 + 0x800"
+#define CT_STRUCTURED_TEXT      13
+#define LB_MULTI 				0x20
